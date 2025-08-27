09d83082c4f0d0b6f4c3519e3853791eee01fa5d
Documenter.jl <documenter@juliadocs.github.io>
2025-08-27 15:01:41 +0000

    build based on 02c4661

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NetworkDesignOptimization","page":"Home","title":"NetworkDesignOptimization","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NetworkDesignOptimization.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
